9559d0d011f5599bbf89e7e8adf68aa22afbba66
ka5769062 <75374110+ka5769062@users.noreply.github.com>
2023-05-20 06:25:01 -0700

    rich text editor

---
+++ index.js
@@ -0,0 +1,121 @@
+
+function mybold(){
+
+    var thicker =  document.getElementById('mytext')
+
+if(thicker.style.fontWeight == "bold"){
+
+    thicker.style.fontWeight = "normal";
+
+
+}
+
+else{
+
+    thicker.style.fontWeight = "bold";
+
+}
+
+
+}
+
+
+function myitalic(){
+
+
+    var thicker =  document.getElementById('mytext')
+
+
+ if(thicker.style.fontStyle == "italic"){
+
+    thicker.style.fontStyle = "normal"
+
+ }
+
+else{
+
+    thicker.style.fontStyle = "italic";
+
+}
+
+ }
+
+
+ function myunderline(){
+
+        var thicker =  document.getElementById('mytext')
+      
+      
+      if(thicker.style.textDecoration == "underline"){
+
+        thicker.style.textDecoration = "none"
+
+      }  
+      else{
+
+        thicker.style.textDecoration = "underline";
+    }
+
+        }    
+
+
+
+function myleft(){
+
+var left = document.getElementById('mytext')
+left.style.textAlign = "left"
+
+if(left.style.textAlign == "left"){
+
+    left.style.textAlign = "none"
+
+  }  
+  else{
+
+    left.style.textAlign = "left";
+}
+
+
+}
+
+
+function mycentre(){
+
+var center = document.getElementById('mytext')
+center.style.textAlign = "center";
+
+if(center.style.textAlign = "center"){
+    
+    center.style.textAlign = "none"
+
+}
+
+else{
+    
+    center.style.textAlign = "center";
+
+}
+
+}
+
+
+
+function myright(){
+
+    var rite = document.getElementById('mytext')
+    rite.style.textAlign = "right";
+    
+    if(rite.style.textAlign = "right"){
+        
+        rite.style.textAlign = "none"
+    
+    }
+    
+    else{
+        
+        rite.style.textAlign = "right";
+    
+    }
+    
+    }
+    
